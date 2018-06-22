0afd4d6f2e5bc11dd5115801f09ba073be06d5e3
Konny <pinchehugochan@gmail.com>
2018-06-22 13:53:17 -0600

    Laboratorio y luz

---
+++ luzmovible/build/CMakeFiles/pelota.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pelota.dir/main.cpp.o"
+  "pelota.pdb"
+  "pelota"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pelota.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
